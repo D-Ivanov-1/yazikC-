b4bf96174a320723e818aa4e2ec7733ba6927de1
Dima Ivanov <ivanovdimat5@gmail.com>
2023-01-06 13:44:26 +0300

    несены правки2

---
+++ Ex004/Program.cs
@@ -0,0 +1,5 @@
+﻿int numberA = new Random().Next(1, 10); // 1 2 3 4 5 ... 9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
